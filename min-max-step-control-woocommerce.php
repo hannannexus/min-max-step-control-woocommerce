1089926204cc681f2f09ec847252bb0de2b23c53
Hannan <hannannexus@gmail.com>
2023-05-20 19:16:05 +0600

    Update min-max-step-control-woocommerce.php
    min max tab added

--- min-max-step-control-woocommerce.php
+++ min-max-step-control-woocommerce.php
@@ -9,3 +9,19 @@
  * Version: 1.0.0
  * Text Domain: min_max_step_control
  */
+
+
+/*Add setting on product tab*/
+if( !function_exists("mmscw_add_new_tab")){
+  function mmscw_add_new_tab( $tab ){
+    $tab["mmscw_tab"] = array(
+      "label"=> __("Min Max Step Control","min_max_step_control"),
+      "target"=>"min_max_step_control_option",
+      "class"=>['hide_if_external'],
+      "priority"=> 25
+    );
+    
+    return  $tab;
+  }
+}
+add_action("woocommerce_product_data_tabs","mmscw_add_new_tab",222,1);
