2ca285b561677af66a1ddaa69e51f0fcf338e4f6
Hannan <hannannexus@gmail.com>
2023-05-20 19:05:51 +0600

    Create min-max-step-control-woocommerce.php
    Plugin head section ready

---
+++ min-max-step-control-woocommerce.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Min Max Setp Control for WooCommerce 
+ * Plugin URI: https://github.com/hannannexus
+ * Description:  Easily Control min max quantity of woocommerce products.
+ * Author: Hannan
+ * Author URI: https://github.com/hannannexus/
+ * License: License: GPLv2 or later
+ * Version: 1.0.0
+ * Text Domain: min_max_step_control
+ */
